baaeca8416177cd6ea8c333dfb22fbc8c4e54956
Manish Kumar <manishkumar.dsi@gmail.com>
2023-04-09 12:47:12 +0530

    Code documentation

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -13,16 +13,31 @@ export class AppService {
   private apiKey = 'cb1508bb97641beb911a5a12ca2c138a';
   constructor(private http: HttpClient,) { }
 
+  /**
+   * Fetch weather data based on coordinates
+   * @param lat Latitude
+   * @param lon Longitude
+   * @returns Observables<WeatherResponse>
+   */
   fetchWeatherByCoordinate(lat: number, lon: number): Observable<WeatherResponse> {
-    console.log(this.openWeatherURL + '?lat=' + lat +'&lon=' + lon + '&units=metric' + '&appid=' + this.apiKey);
-    // return this.http.get('${this.openWeatherURL}?lat=${lat}&lon=${lon}&appid=${this.apiKey}`);
     return this.http.get<WeatherResponse>(this.openWeatherURL + '?lat=' + lat +'&lon=' + lon + '&units=metric' +  '&appid=' + this.apiKey);
   }
 
+  /**
+   * Fetch weather data based on city
+   * @param cityName city name
+   * @returns Observables<WeatherResponse>
+   */
   fetchWeatherByCity(cityName: string): Observable<WeatherResponse> {
     return this.http.get<WeatherResponse>(this.openWeatherURL + '?q=' + cityName  + '&units=metric' + '&appid=' + this.apiKey);
   }
 
+  /**
+   * Fetch Irradiance data based on coordinates
+   * @param lat Latitude
+   * @param lon Longitude
+   * @returns Observable<GraphData>
+   */
   fetchGraphData(lat: number, lon: number): Observable<GraphData> {
     return this.http.get<GraphData>('https://manishkumardsi.pythonanywhere.com?lat=' + lat + '&lon=' + lon);
   }
